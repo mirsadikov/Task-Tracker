9ff5cfc037d83f0d795a95372e9184992058defb
mirsadikov <mirabror9545@gmail.com>
2021-04-20 15:41:25 +0500

    HEader and Button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
@@ -13,7 +18,7 @@ Header.defaultProps = {
 };
 
 Header.propTypes = {
-  title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
